c4a5e1ad54f1094229b8eb00f5b07f9980d29371
PhineasFreak <precise.timing.labs@gmail.com>
2017-10-01 21:23:40 +0300

    Update the dev environment (again...)
    SharpDevelop is just a much better IDE than Visual Studio.
    DICE...umm...Microsoft...pls™!

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 //  ================================================================================
 //  Real Solar System Visual Enhancements for Kerbal Space Program.
 
-//  Copyright © 2016-2017, Alexander "Phineas Freak" Kampolis.
+//  Copyright Â© 2016-2017, Alexander "Phineas Freak" Kampolis.
 
 //  This file is part of Real Solar System Visual Enhancements.
 
@@ -11,39 +11,37 @@
 //  You should have received a copy of the license along with this work. If not, visit the official
 //  Creative Commons web page:
 
-//      • https://www.creativecommons.org/licensies/by-nc-sa/4.0
+//      â€¢ https://www.creativecommons.org/licensies/by-nc-sa/4.0
 //  ================================================================================
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 //  General Information about the specified assembly.
 
-[assembly: AssemblyCompany("Phineas Freak")]
-[assembly: AssemblyConfiguration("Stable")]
-[assembly: AssemblyCopyright("Copyright © 2016 - 2017, Phineas Freak")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyDescription("Real Solar System Visual Enhancements")]
-[assembly: AssemblyProduct("RSSVE")]
-[assembly: AssemblyTitle("RSSVE")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany ("Phineas Freak")]
+[assembly: AssemblyConfiguration ("Stable")]
+[assembly: AssemblyCopyright ("Copyright Â© 2016 - 2017, Phineas Freak")]
+[assembly: AssemblyCulture ("")]
+[assembly: AssemblyDescription ("Real Solar System Visual Enhancements")]
+[assembly: AssemblyProduct ("RSSVE")]
+[assembly: AssemblyTitle ("RSSVE")]
+[assembly: AssemblyTrademark ("")]
 
 //  Hide the specified assembly from any COM components.
 
-[assembly: ComVisible(false)]
+[assembly: ComVisible (false)]
 
 //  Version information for the specified assembly.
-
 //  Consists of the following four values:
 //
-//    • Major Version
-//    • Minor Version
-//    • Build Number
-//    • Revision
+//    â€¢ Major Version
+//    â€¢ Minor Version
+//    â€¢ Build Number
+//    â€¢ Revision
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.1804")]
-[assembly: AssemblyInformationalVersion("1.3.0.1804")]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.3.0.1804")]
+[assembly: AssemblyInformationalVersion ("1.3.0.1804")]
 
-[assembly: KSPAssembly("RSSVE", 1, 0)]
+[assembly: KSPAssembly ("RSSVE", 1, 0)]
